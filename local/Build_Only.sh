befc007c1ee7494cae6110d60fe972caa833b709
p0s3id0n <apollo13686@gmail.com>
2026-04-25 21:07:33 +0700

    Up v2.1

--- local/Build_Only.sh
+++ local/Build_Only.sh
@@ -94,7 +94,7 @@ sudo rm -rf kernel_workspace
 mkdir -p kernel_workspace && cd kernel_workspace
 
 echo "🌐 正在初始化 oneplus/${CPU} 分支、机型 ${FEIL} 的 manifest..."
-repo init -u https://github.com/Xiaomichael/kernel_manifest.git -b refs/heads/oneplus/${CPU} -m ${FEIL}.xml --depth=1
+repo init -u https://github.com/p0s3id0n-ops/kernel_manifest.git -b refs/heads/oneplus/${CPU} -m ${FEIL}.xml --depth=1
 
 echo "🔄 正在同步内核源码仓库 (使用 $(nproc --all) 线程)..."
 repo sync -c -j$(nproc --all) --no-tags --no-clone-bundle --force-sync
@@ -113,11 +113,11 @@ sed -i '$i res=$(echo "$res" | sed '\''s/-dirty//g'\'')' kernel_platform/msm-ker
 sed -i '$i res=$(echo "$res" | sed '\''s/-dirty//g'\'')' kernel_platform/external/dtc/scripts/setlocalversion
 
 if [ "$KERNEL_VERSION" != "6.6" ]; then
-  sed -i '$s|echo "\$res"|echo "-'"$adv"'-oki-xiaoxiaow"|' kernel_platform/common/scripts/setlocalversion
-  sed -i '$s|echo "\$res"|echo "-'"$adv"'-oki-xiaoxiaow"|' kernel_platform/msm-kernel/scripts/setlocalversion
-  sed -i '$s|echo "\$res"|echo "-'"$adv"'-oki-xiaoxiaow"|' kernel_platform/external/dtc/scripts/setlocalversion
+  sed -i '$s|echo "\$res"|echo "-'"$adv"'-o-p0s3id0n"|' kernel_platform/common/scripts/setlocalversion
+  sed -i '$s|echo "\$res"|echo "-'"$adv"'-o-p0s3id0n"|' kernel_platform/msm-kernel/scripts/setlocalversion
+  sed -i '$s|echo "\$res"|echo "-'"$adv"'-o-p0s3id0n"|' kernel_platform/external/dtc/scripts/setlocalversion
 else
-  ESCAPED_SUFFIX=$(printf '%s\n' "-${ANDROID_VERSION}-oki-xiaoxiaow" | sed 's:[\/&]:\\&:g')
+  ESCAPED_SUFFIX=$(printf '%s\n' "-${ANDROID_VERSION}-o-p0s3id0n" | sed 's:[\/&]:\\&:g')
   sed -i "s/-4k/${ESCAPED_SUFFIX}/g" kernel_platform/common/arch/arm64/configs/gki_defconfig
   sed -i 's/\${scm_version}//' kernel_platform/common/scripts/setlocalversion
   sed -i 's/\${scm_version}//' kernel_platform/msm-kernel/scripts/setlocalversion
@@ -136,7 +136,7 @@ if [ "$bbg" = "On" ]; then
 fi
 
 echo "📝 正在复制补丁文件..."
-git clone https://github.com/Xiaomichael/kernel_patches.git
+git clone https://github.com/p0s3id0n-ops/kernel_patches.git
 git clone https://github.com/ShirkNeko/SukiSU_patch.git
 
 cd kernel_platform
@@ -307,8 +307,8 @@ cd "$WORKSPACE/kernel_workspace/kernel_platform/common"
 
 MAKE_CMD_COMMON="make -j$(nproc --all) LLVM=1 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CC=\"ccache clang\" RUSTC=../../prebuilts/rust/linux-x86/1.73.0b/bin/rustc PAHOLE=../../prebuilts/kernel-build-tools/linux-x86/bin/pahole LD=ld.lld HOSTLD=ld.lld O=out gki_defconfig all"
 
-export KBUILD_BUILD_USER="xiaoxiaow"
-export KBUILD_BUILD_HOST="xiaoxiaow_build"
+export KBUILD_BUILD_USER="p0s3id0n"
+export KBUILD_BUILD_HOST="p0s3id0n_build"
 
 if [ "$KERNEL_VERSION" = "6.1" ]; then
     export KBUILD_BUILD_TIMESTAMP="Tue Mar 10 03:53:33 UTC 2026"
@@ -335,7 +335,7 @@ echo "✅ 内核编译完成"
 cd "$WORKSPACE"
 
 echo "📦 正在获取 AnyKernel3 并准备打包..."
-git clone https://github.com/Xiaomichael/AnyKernel3 --depth=1
+git clone https://github.com/p0s3id0n-ops/AK3-p0d3id0n-ops --depth=1
 rm -rf ./AnyKernel3/.git
 
 IMAGE_PATH=$(find "$WORKSPACE/kernel_workspace/kernel_platform/common/out/" -name "Image" | head -n 1)
